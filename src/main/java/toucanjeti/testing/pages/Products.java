dbb5470c1f1a61452820b5b1f27bbe1e8aeb6843
Csege <cseloteigergely@gmail.com>
2024-01-03 13:36:01 +0100

    add webelement locators and by-s to Products, add getNumberOfProducts and clickProductButton methods to Products

--- src/main/java/toucanjeti/testing/pages/Products.java
+++ src/main/java/toucanjeti/testing/pages/Products.java
@@ -1,9 +1,11 @@
 package toucanjeti.testing.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -17,6 +19,19 @@ public class Products {
     //inventory items
     @FindBy(xpath = "//div[@class='inventory_item']")
     private List<WebElement> products;
+    //item filter
+    @FindBy(className = "product_sort_container")
+    private Select filterSelect;
+    //shopping cart & shopping cart counter
+    @FindBy(className = "shopping_cart_link")
+    private WebElement shoppingButton;
+    @FindBy(className = "shopping_cart_badge")
+    private WebElement shoppingButtonBadge;
+    //By-s for item datas
+    private static final By productName = By.className("inventory_item_name");
+    private static final By productPrice = By.className("inventory_item_price");
+    private static final By productDescription = By.className("inventory_item_desc");
+    private static final By productButton = By.className("btn_inventory");
 
     public Products(WebDriver driver) {
         this.driver = driver;
@@ -27,5 +42,12 @@ public class Products {
     }
     public void pressLogoutButton() {
         logoutButton.click();
+    }
+    public int getNumberOfProducts() {
+        return products.size();
+    }
+    public String clickProductButton(int productNumber) {
+        products.get(productNumber).findElement(productButton).click();
+        return products.get(productNumber).findElement(productName).getText();
     }
 }
