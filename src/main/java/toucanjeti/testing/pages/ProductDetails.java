fd5f1f0efe2fff5e63126760317950c87bec4508
Csege <cseloteigergely@gmail.com>
2024-01-04 09:27:56 +0100

    add clickShpooingButton and getShoppingBadgeValue methods to Products, add ProductDetails POM

---
+++ src/main/java/toucanjeti/testing/pages/ProductDetails.java
@@ -0,0 +1,12 @@
+package toucanjeti.testing.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductDetails {
+    private WebDriver driver;
+
+
+    public ProductDetails(WebDriver driver) {
+        this.driver = driver;
+    }
+}
