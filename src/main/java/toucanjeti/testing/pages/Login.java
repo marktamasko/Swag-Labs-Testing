eda2da10a0713e4e2638c8f8a36c4514972388f5
marktamasko <25462245+marktamasko@users.noreply.github.com>
2024-01-04 10:27:22 +0100

    feat[Login]: add error message field

--- src/main/java/toucanjeti/testing/pages/Login.java
+++ src/main/java/toucanjeti/testing/pages/Login.java
@@ -21,6 +21,9 @@ public class Login {
     @FindBy(id = "login-button")
     private WebElement loginButton;
 
+    @FindBy(xpath = "//*[@id=\"login_button_container\"]/div/form/div[3]/h3")
+    private WebElement errorMessage;
+
     public Login(WebDriver webDriver) {
         PageFactory.initElements(webDriver, this);
         wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
@@ -40,4 +43,12 @@ public class Login {
         this.loginButton.click();
     }
 
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+
+    public void clearLoginField() {
+        this.usernameField.clear();
+        this.passwordField.clear();
+    }
 }
