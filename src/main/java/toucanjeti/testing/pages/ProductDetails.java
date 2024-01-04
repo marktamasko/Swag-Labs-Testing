c44f546355ef832b5b93d5560dace5eb44284b82
Csege <cseloteigergely@gmail.com>
2024-01-04 10:01:19 +0100

    add locators and getters to ProductDetails POM

--- src/main/java/toucanjeti/testing/pages/ProductDetails.java
+++ src/main/java/toucanjeti/testing/pages/ProductDetails.java
@@ -1,12 +1,50 @@
 package toucanjeti.testing.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
 
 public class ProductDetails {
     private WebDriver driver;
-
+    @FindBy(className = "inventory_details_name")
+    private WebElement itemName;
+    @FindBy(className = "inventory_details_desc")
+    private WebElement itemDescription;
+    @FindBy(className = "inventory_details_price")
+    private WebElement itemPrice;
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    private WebElement addToCartButton;
 
     public ProductDetails(WebDriver driver) {
         this.driver = driver;
+    }
+    private void useFluentWait(WebElement element) {
+        Wait<WebDriver> driverWait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+        driverWait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public String getItemName() {
+        useFluentWait(itemName);
+        return itemName.getText();
+    }
+    public String getItemDescription() {
+        useFluentWait(itemDescription);
+        return itemDescription.getText();
+    }
+    public String getItemPrice() {
+        useFluentWait(itemPrice);
+        return itemPrice.getText();
+    }
+    public String getButtonText() {
+        useFluentWait(addToCartButton);
+        return addToCartButton.getText();
     }
 }
